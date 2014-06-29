5c2e7c8520ec9016d63774f7ca6cad71ecaceefe
moralesalberto <alberto@moralitos.com>
2014-06-29 12:41:19 -0400

    adding the showing and hiding of the stickyPad

--- sticky_notes.safariextension/safari/background.js
+++ sticky_notes.safariextension/safari/background.js
@@ -6,6 +6,18 @@ var backgroundObject = function () {
 
   var _stickyPadIsVisible = false;
 
+  // iterate through all the windows, all the tabs in the window
+  // and send a message to the tab to close the stickyPad
+  var _closeStickyPadInAllWindows = function () {
+    for(var i = 0; i < safari.application.browserWindows.length; i++) {
+      var currentWindow = safari.application.browserWindows[i];
+      for(var j=0; j < currentWindow.tabs.length; j++) {
+        var tab = currentWindow.tabs[j];
+        tab.page.dispatchMessage('hideStickyPad', 'hideStickyPad');
+      }
+    }
+  };
+
   var _handleCommand = function (event) {
     // the user clicked on the button in the toolbar
     // in the extension builder the command string is set to 'toggleNotepadVisibility'
@@ -15,9 +27,10 @@ var backgroundObject = function () {
 
 
       // tell all windows and tabs to hide the stickyPad
-      // we only show the stickyPad in the activeTab; we do not show more than one instance
+      _closeStickyPadInAllWindows();
 
       // tell the active tab to show the stickyPad if wanting visibility
+      // we only show the stickyPad in the activeTab
       if(_stickyPadIsVisible === true) {
         var theActiveWindow = safari.application.activeBrowserWindow;
         theActiveWindow.activeTab.page.dispatchMessage('showStickyPad', 'showStickyPad');
