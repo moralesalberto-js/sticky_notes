65ba579487d38d259161880c0b005bb7d48197d1
Mickael <m.coenca@gmail.com>
2014-07-01 14:11:26 -0400

    ssmall changes

--- sticky_notes.safariextension/firefox/lib/firefox_browser_adapter.js
+++ sticky_notes.safariextension/firefox/lib/firefox_browser_adapter.js
@@ -8,24 +8,25 @@ var browser = function () {
   var _backgroundCommandsListenerFunction;
   var _backgroundMessagesListenerFunction;
 
-  // we receive the event in Safari native signature
+  // we receive the event in Chrome way
   // we translate it to the two variables in the shared interface
   // _function(message_name, message_data)
   var _injectedRespondToMessages = function(theMessageEvent) {
-    _injectedMessagesListenerFunction(theMessageEvent.name, theMessageEvent.message);
+    _injectedMessagesListenerFunction(theMessageEvent.name, theMessageEvent.data);
   };
 
   // this receives the command (click of toolbar) by Safari
   // sends it translated to the signature of the generic response to command
   // _function(command_name)
+// !!!!!!! Perhaps optional command Data ?
   var _backgroundRespondToCommands = function(theCommandEvent) {
-    _backgroundCommandsListenerFunction(theCommandEvent.command);
+    _backgroundCommandsListenerFunction(theCommandEvent.name);
   };
 
   // this function receives the messages from the injected script
   var _backgroundRespondToMessages = function(theMessageEvent){
     var message_name = theMessageEvent.name;
-    var message_data = theMessageEvent.message;
+    var message_data = theMessageEvent.data;
     _backgroundMessagesListenerFunction(message_name, message_data);
   };
 
@@ -36,6 +37,8 @@ var browser = function () {
   var self = {
 
     // function to retun an array of all the currently open tabs
+
+// !!!!!! Callback in chrome
     getAllTabs: function () {
       var _allTabs = [];
       for(var i = 0; i < safari.application.browserWindows.length; i++) {
@@ -46,33 +49,42 @@ var browser = function () {
         }
       }
       return _allTabs;
+      // return chrome.tabs.query
     },
 
+// !!!!!! Callback in chrome
     getActiveTab: function () {
-      return safari.application.activeBrowserWindow.activeTab;
+      //return chrome.tabs.query({active: true, lastFocusedWindow: true},function(tabs){
+
+      //});
     },
 
     // send a message to a specific tab
     sendMessageToTab: function(tab, message, data) {
-      tab.page.dispatchMessage(message, data);
+      chrome.tabs.sendMessage(tab.id, {name: message, data: data});
+      chrome.runtime.sendMessage({name: message, data: data});
     },
 
     // send a message from injected script to background js
     sendMessageToBackground: function(message, data) {
-      safari.self.tab.dispatchMessage(message, data);
+      chrome.runtime.sendMessage({name: message, data: data});
     },
 
     // the adapter listener for messages
+    // (This function is called once by page)
+    // functiontoCall(message_name, message_data)
     addInjectedMessagesListener: function(functionToCall) {
       _injectedMessagesListenerFunction = functionToCall;
-      safari.self.addEventListener("message", _injectedRespondToMessages, false);
+      chrome.runtime.onMessage.addListener(_injectedRespondToMessages);
     },
 
     // assign the command passed to our variable to call later
     // register the command listener
+    // functionToCall (message_name, message_data)
+// !!!!!! Not same distinction in chrome, check global.js
     addBackgroundCommandsListener: function(functionToCall) {
       _backgroundCommandsListenerFunction = functionToCall;
-      safari.application.addEventListener("command", _backgroundRespondToCommands, false);
+      chrome.runtime.onMessage.addListener(_backgroundRespondToCommands);
     },
 
     // similar to commands, assign the function to call
@@ -80,19 +92,20 @@ var browser = function () {
     // and translate it to the generic form
     addBackgroundMessagesListener: function(functionToCall) {
       _backgroundMessagesListenerFunction = functionToCall;
-      safari.application.addEventListener("message", _backgroundRespondToMessages, false);
+      chrome.runtime.onMessage.addListener(_backgroundRespondToMessages);
     },
 
     // This is the URL to access files that are in the extension directory
     // It is used to access the haml templates for example.
     getLocalUrlFor: function(relative_path) {
-      return (safari.extension.baseURI + relative_path);
+      return chrome.extension.getURL(relative_path);
     },
 
     // LOCAL STORAGE
     // local storage works with simple key, value stores
     // For this exercise we are only saving a single note
     // We can add pagination later on to have multiple notes
+// !!!!!!! The specific and better chrome storage is Asynchronous
     saveToLocalStorage: function(data) {
       localStorage[data.key] = data.value;
     },
