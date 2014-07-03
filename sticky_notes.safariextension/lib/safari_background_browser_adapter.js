a1a175f1a0156b372fe7deeb65a12827a50fd51d
Mickael <m.coenca@gmail.com>
2014-07-03 16:54:22 -0400

    safari extension working

--- sticky_notes.safariextension/lib/safari_background_browser_adapter.js
+++ sticky_notes.safariextension/lib/safari_background_browser_adapter.js
@@ -1,19 +1,11 @@
 // This is the adapater object that will wrap all the safari specific calls
 // Use the same interface for the other browsers
 
-var browser = function () {
+var background_adapter = function () {
   // this is the var that will hold the function the extension registered
   // as the function to listen for injected scripts
-  var _injectedMessagesListenerFunction;
   var _backgroundCommandsListenerFunction;
   var _backgroundMessagesListenerFunction;
-
-  // we receive the event in Safari native signature
-  // we translate it to the two variables in the shared interface
-  // _function(message_name, message_data)
-  var _injectedRespondToMessages = function(theMessageEvent) {
-    _injectedMessagesListenerFunction(theMessageEvent.name, theMessageEvent.message);
-  };
 
   // this receives the command (click of toolbar) by Safari
   // sends it translated to the signature of the generic response to command
@@ -33,10 +25,11 @@ var browser = function () {
   // PUBLIC API
   // if you change any of these functions, you need to visit the implementation for all
   // browsers and add your changes there as well
-  var self = {
+  var self_ = {
 
     // function to retun an array of all the currently open tabs
-    getAllTabs: function () {
+    //calllback must be a function([tabs])
+    getAllTabs: function (callback) {
       var _allTabs = [];
       for(var i = 0; i < safari.application.browserWindows.length; i++) {
         var currentWindow = safari.application.browserWindows[i];
@@ -45,27 +38,17 @@ var browser = function () {
           _allTabs.push(tab);
         }
       }
-      return _allTabs;
+      callback(_allTabs);
     },
 
-    getActiveTab: function () {
-      return safari.application.activeBrowserWindow.activeTab;
+    //callback must be a function(tab)
+    getActiveTab: function (callback) {
+      callback(safari.application.activeBrowserWindow.activeTab);
     },
 
     // send a message to a specific tab
     sendMessageToTab: function(tab, message, data) {
       tab.page.dispatchMessage(message, data);
-    },
-
-    // send a message from injected script to background js
-    sendMessageToBackground: function(message, data) {
-      safari.self.tab.dispatchMessage(message, data);
-    },
-
-    // the adapter listener for messages
-    addInjectedMessagesListener: function(functionToCall) {
-      _injectedMessagesListenerFunction = functionToCall;
-      safari.self.addEventListener("message", _injectedRespondToMessages, false);
     },
 
     // assign the command passed to our variable to call later
@@ -79,6 +62,7 @@ var browser = function () {
     // to a variable so that we can call it after we receive the message
     // and translate it to the generic form
     addBackgroundMessagesListener: function(functionToCall) {
+      console.log("adding bckground rules");
       _backgroundMessagesListenerFunction = functionToCall;
       safari.application.addEventListener("message", _backgroundRespondToMessages, false);
     },
@@ -102,7 +86,7 @@ var browser = function () {
     }
   };
 
-  return self;
+  return self_;
 
 }.call();
 
--- sticky_notes.safariextension/lib/shared/background.js
+++ sticky_notes.safariextension/lib/shared/background.js
@@ -9,10 +9,10 @@ var background = function () {
 
     // send a message to all the windows and tabs to close the sticky notepad
     var _hideInAllTabs = function () {
-      browser.getAllTabs(function(_tabs){
+      background_adapter.getAllTabs(function(_tabs){
         for(var i = 0; i < _tabs.length; i++) {
           var _tab = _tabs[i];
-          browser.sendMessageToTab(_tab, 'hideStickyPad', 'hideStickyPad');
+          background_adapter.sendMessageToTab(_tab, 'hideStickyPad', 'hideStickyPad');
         }
       });
     };
@@ -22,8 +22,9 @@ var background = function () {
       var _data = { html: _getHtmlForView() };
       // we send a message to the injected script with the html
       // to paint the view
-      browser.getActiveTab(function(_tab){
-        browser.sendMessageToTab(_tab, 'showStickyPad', _data);
+      background_adapter.getActiveTab(function(_tab){
+        console.log("sending the message to injected");
+        background_adapter.sendMessageToTab(_tab, 'showStickyPad', _data);
       });
     };
 
@@ -31,14 +32,15 @@ var background = function () {
     // this function gets the html from the haml template
     // and whatever variables need to be filled in the template
     var _getHtmlForView = function () {
-      var _templateUrl = browser.getLocalUrlFor("shared/templates/sticky_pad.html.haml");
+      var _templateUrl = background_adapter.getLocalUrlFor("data/shared/templates/sticky_pad.html.haml");
       var _template = haml.compileHaml( { sourceUrl: _templateUrl } );
 
       // get the note saved in local storage
       // if there is none, then just return a default string
-      var _note_content = browser.getDataFromLocalStorageForKey('note_content') || 'Enter you notes here ...';
+      var _note_content = background_adapter.getDataFromLocalStorageForKey('note_content') || 'Enter you notes here ...';
       var _vars = {content: _note_content};
       var _compiledHtml = _template(_vars);
+      console.log("Haml"+_compiledHtml);
       return _compiledHtml;
     };
 
@@ -57,23 +59,24 @@ var background = function () {
 
   var _handleCommands = function (command_name) {
     if(command_name === 'showStickyPad') {
+      console.log("Button clicked");
       _stickyPad.show();
     }
   };
 
   var _setupCommandsListener = function() {
-    browser.addBackgroundCommandsListener(_handleCommands);
+    background_adapter.addBackgroundCommandsListener(_handleCommands);
   };
 
 
   var _handleMessages = function(message_name, message_data) {
     if(message_name === 'saveNoteContent') {
-      browser.saveToLocalStorage({key: 'note_content', value: message_data.content});
+      background_adapter.saveToLocalStorage({key: 'note_content', value: message_data.content});
     }
   };
 
   var _setupMessagesListener = function () {
-    browser.addBackgroundMessagesListener(_handleMessages);
+    background_adapter.addBackgroundMessagesListener(_handleMessages);
   };
 
 
